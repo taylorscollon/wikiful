00cc337eeaf557e7ea5eaa5a331c77fe7659feca
Taylor Scollon <taylor@adrenalineco.com>
2014-08-20 15:30:20 -0400

    created Article model

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+  belongs_to :users
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :categories, presence: true
+end
