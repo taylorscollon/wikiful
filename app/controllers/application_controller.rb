965410a61585b2ba9e29919d7b1691c0742d2ba2
Taylor Scollon <taylor@adrenalineco.com>
2014-08-24 11:36:57 -0400

    added user login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,12 @@ class ApplicationController < ActionController::Base
   def get_categories
     @categories = Category.all.order(:name)
   end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end 
+  def authorize
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end 
+  helper_method :current_user
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles, :categories, :users
+  resources :articles, :categories, :users, :sessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
